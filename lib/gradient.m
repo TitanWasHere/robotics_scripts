009ecca036f28c0a672c5ab091e1a9816c640c87
TitanWasHere <andrea.gravili8@gmail.com>
2025-01-14 00:28:19 +0100

    IK methods

--- lib/gradient.m
+++ lib/gradient.m
@@ -1,13 +1,14 @@
-function [q_out, guesses, cartesian_errors] = newton(q_in, desired_point, f_r, initial_guess, max_iterations, max_cartesian_error, min_joint_increment, max_closeness_singularity)
-% [q_out, guesses, cartesian_errors] = newton(q_in, desired_point, f_r, 
-%  initial_guess, max_iterations, max_cartesian_error, min_joint_increment, 
-%  max_closeness_singularity) takes as inputs:
+function [q_out, guesses, cartesian_errors] = gradient(q_in, desired_point, f_r, initial_guess, max_iterations, max_cartesian_error, learning_rate, min_joint_increment, max_closeness_singularity)
+% [q_out, guesses, cartesian_errors] = gradient_method(q_in, desired_point, f_r, 
+%  initial_guess, max_iterations, max_cartesian_error, learning_rate, 
+%  min_joint_increment, max_closeness_singularity) takes as inputs:
 %   -q_in: The variables of the joints, e.g. [q1 q2 q3 q4]
 %   -desired_point: the configuration we wish to reach
 %   -f_r: The mapping from joints to points
 %   -initial_guess: Initial configuration of joints
 %   -max_iterations: The maximum number of iterations we can perform
 %   -max_cartesian_error: The level of precision we wish to reach
+%   -learning_rate: The step size to move along the gradient direction
 %   -min_joint_increment: (Optional) The minimum level of increment of accuracy 
 %                          between successive iterations. Default = 1e-5.
 %   -max_closeness_singularity: (Optional) How close to a singularity we can get.
@@ -24,57 +25,39 @@ function [q_out, guesses, cartesian_errors] = newton(q_in, desired_point, f_r, i
     if nargin < 8
         max_closeness_singularity = 1e-4;
     end
-
+    
     % Compute the Jacobian
     J = jacobian(f_r, q_in);
     
-    % Check system type (redundant or non-redundant)
-    n_dof = length(q_in);
-    n_config = length(desired_point);
-    
-    simple_case = n_config == n_dof;
-    
-    if n_config < n_dof
-        disp("Redundant case, let's use the pseudo inverse");
-        J_inv = pinv(J);
-    else
-        disp("Non-redundant case, let's use the inverse");
-        J_inv = inv(J);
-    end
-    
     % Initialize variables
-    guesses = zeros(max_iterations + 1, n_dof);
+    guesses = zeros(max_iterations + 1, length(q_in));
     cartesian_errors = zeros(1, max_iterations + 1);
     guess = initial_guess;
     
-    % Iterative Newton-Raphson method
+    % Iterative gradient descent method
     for i = 1:max_iterations
         guesses(i, :) = guess;
         
-        % Compute Cartesian error
+        % Compute the Cartesian error
         error = norm(desired_point - subs(f_r, q_in, guess));
         cartesian_errors(i) = error;
+        
         if error < max_cartesian_error
             fprintf("Finished at iteration %d because the error was lower than the specified amount.\n", i);
             break
         end
         
         % Check for singularity
-        if simple_case
-            if abs(det(subs(J, q_in, guess))) <= max_closeness_singularity
-                fprintf("Finished at iteration %d because too close to a singularity.\n", i);
-                break
-            end
-        else
-            [~, D, ~] = svd(subs(J, q_in, guess));
-            if min(min(D)) >= D(1, 1) - max_closeness_singularity
-                fprintf("Finished at iteration %d because too close to a singularity.\n", i);
-                break
-            end
+        if abs(det(subs(J, q_in, guess))) <= max_closeness_singularity
+            fprintf("Finished at iteration %d because too close to a singularity.\n", i);
+            break
         end
         
-        % Compute the next guess
-        new_guess = guess + subs(J_inv, q_in, guess) * (desired_point - subs(f_r, q_in, guess));
+        % Compute the gradient of the error w.r.t. the joint angles
+        error_gradient = -subs(J, q_in, guess)' * (desired_point - subs(f_r, q_in, guess));
+        
+        % Update the guess using the gradient descent rule
+        new_guess = guess + learning_rate * error_gradient';
         
         % Check for minimal increment
         if norm(new_guess - guess) <= min_joint_increment
@@ -82,7 +65,7 @@ function [q_out, guesses, cartesian_errors] = newton(q_in, desired_point, f_r, i
             break
         end
         
-        guess = eval(new_guess);
+        guess = new_guess;
     end
     
     % Trim outputs
